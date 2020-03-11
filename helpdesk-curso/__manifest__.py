e874bfa4d2493f747a872353fc3c0a669a0e9567
manuel-sw <manueldelosreyes@rrhhsecondway.com>
2020-03-11 17:40:58 +0100

    [ADD] Base ticket

---
+++ helpdesk-curso/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "HelpDesk Curso",
+    'summary':
+        "Module to support teams",
+    'version': '13.0.1.0',
+    'category': 'Customer relationship management',
+    'website': "",
+    'author': "manuel-sw",
+    'license' : "AGPL-3",
+    'data': [
+        "security/helpdesk_security.xml",
+        "security/ir.model.access.csv",
+        "views/helpdesk_ticket_views.xml",
+    ],
+    'application': True,
+    'installable': True,
+}
